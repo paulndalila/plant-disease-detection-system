2abe579555197e1394b176342da1457603378ed6
Paul Ndalila <paulndalila001@gmail.com>
2024-03-20 00:56:58 +0300

    backend update

--- fastapi/app.py
+++ fastapi/app.py
@@ -51,4 +51,4 @@ async def predict(file: UploadFile = File(...)):
     }
 
 if __name__ == "__main__":
-    uvicorn.run(app, host='10.0.9.237', port=8000)
+    uvicorn.run(app, host='localhost', port=8000)
